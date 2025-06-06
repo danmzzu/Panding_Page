5cede84c97d6165b4ebd838504a8fafe84e67fdd
L009 <danmzzu@gmail.com>
2025-06-06 18:21:35 -0300

    Update

---
+++ scripts/effects.js
@@ -0,0 +1,30 @@
+// Fade In Cards
+document.addEventListener('DOMContentLoaded', () => {
+    const cards = document.querySelectorAll('.about, .service, .feedback, .gallery, .social, .contact, .footer, .category');
+    let lastScrollY = window.scrollY;
+
+    const observer = new IntersectionObserver(entries => {
+        const currentScrollY = window.scrollY;
+        const isScrollingDown = currentScrollY > lastScrollY;
+
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                if (isScrollingDown) {
+                    entry.target.classList.add('visible-from-bottom');
+                    entry.target.classList.remove('visible-from-top');
+                } else {
+                    entry.target.classList.add('visible-from-top');
+                    entry.target.classList.remove('visible-from-bottom');
+                }
+            } else {
+                entry.target.classList.remove('visible-from-bottom');
+                entry.target.classList.remove('visible-from-top');
+            }
+        });
+        lastScrollY = currentScrollY;
+    }, { threshold: 0.1 });
+
+    cards.forEach(card => {
+        observer.observe(card);
+    });
+});
